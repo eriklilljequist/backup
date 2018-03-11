78390a1044697c7c49c2f4516292da65dbab2abc
Erik Thorstenson <erikthorstenson@Eriks-iMac.local>
2018-03-11 13:53:02 +0100

    .

--- do_backup.sh
+++ do_backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # hostname=`hostname`
-
 # aws s3 create-bucket --bucket $hostname
 # echo 'will backup to bucket `aws s3 ls | grep $host-name`'
 
-> ~/folders_to_back_up.backup
-find ~/test -type f -name .dobackup | xargs -L 1 dirname >> ~/folders_to_back_up.backup
+find ~/ -type f -name .dobackup | sed 's#//#/#' | xargs -L 1 dirname > ~/.folders_to_back_up
 
 echo 'will backup folders:'
-cat ~/folders_to_back_up.backup
+cat ~/.folders_to_back_up
 
-sed -i -E 's@^\(.*\)@aws s3 sync \1 s3://BUCKET_NAME\1@g' ~/folders_to_back_up.backup
-source ~/folders_to_back_up.backup
+# Specify --delete to delete files that only exist in target
+sed -i -E 's@^\(.*\)@aws s3 sync --exclude "^.*" \1 s3://{BUCKET_NAME}\1 --delete@g' ~/.folders_to_back_up
+source ~/.folders_to_back_up
+echo 'done'
+exit 0
